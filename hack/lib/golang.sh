df9e35cedd3fb044a68ff086856f29479101e079
shanjiantao <znley@loongson.cn>
2025-02-17 14:18:47 +0800

    patch for scripts

--- hack/lib/golang.sh
+++ hack/lib/golang.sh
@@ -27,6 +27,7 @@ readonly KUBE_SUPPORTED_SERVER_PLATFORMS=(
   linux/arm64
   linux/s390x
   linux/ppc64le
+  linux/loong64
 )
 
 # The node platforms we build for
@@ -36,6 +37,7 @@ readonly KUBE_SUPPORTED_NODE_PLATFORMS=(
   linux/arm64
   linux/s390x
   linux/ppc64le
+  linux/loong64
   windows/amd64
 )
 
@@ -48,6 +50,7 @@ readonly KUBE_SUPPORTED_CLIENT_PLATFORMS=(
   linux/arm64
   linux/s390x
   linux/ppc64le
+  linux/loong64
   darwin/amd64
   darwin/arm64
   windows/amd64
@@ -211,7 +214,8 @@ kube::golang::setup_platforms() {
     host_arch=$(kube::util::host_arch)
     if [[ "${host_arch}" != "amd64" && "${host_arch}" != "arm64" ]]; then
       # on any platform other than amd64 and arm64, we just default to amd64
-      host_arch="amd64"
+      # make quick-release will failed if default to amd64
+      host_arch=$host_arch
     fi
     KUBE_SERVER_PLATFORMS=("linux/${host_arch}")
     readonly KUBE_SERVER_PLATFORMS
